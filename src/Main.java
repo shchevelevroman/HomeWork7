24d62a088308bcabf6f6cfc6f1ec48acc3dbdd90
a123 <shchevelev.roman@yandex.ru>
2022-11-22 20:33:58 +0300

    Задачи 1-4 Выполнены

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задача №1
+        System.out.println("Задача №1");
+        for ( int i = 0; i <= 10; i++){
+            System.out.println(i);
+        }
+        //Задача №2
+        System.out.println("Задача №2");
+        for ( int s = 10; s >= 1; s--){
+            System.out.println(s);
+        }
+        //Задача №3
+        System.out.println("Задача №3");
+        for ( int q = 0; q < 17; q = q+2){
+            System.out.println(q);
+        }
+        //Задача №4
+        System.out.println("Задача №4");
+        for ( int w = 10; w >= -10; w--){
+            System.out.println(w);
+        }
     }
 }
